d479e066f16b728ec4363a5f3a74f6275295dbc2
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2022-06-25 10:38:12 +0800

    fix: vercel deployment issue

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 module.exports = {
 	i18n: {
 		locales: ['en', 'fr', 'es', 'ro', 'zh', 'ar'],
 		defaultLocale: 'en',
+		localePath: path.resolve('./public/static/locales'),
 	},
 }
